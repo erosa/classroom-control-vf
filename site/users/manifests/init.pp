dad42f05da95fb12e59808ca261ff479ae675146
Liz Rosa <liz.rosa@puppet.com>
2016-07-21 21:56:58 +0000

    updated users class with managed_users

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,4 +2,19 @@ class users {
   user { 'fundamentals':
     ensure => present,
   }
+
+  managed_user { 'bob':
+    username => 'bob',
+    ensure   => present,
+  }
+
+  managed_user { 'alice':
+    username => 'alice',
+    ensure   => present,
+  }
+
+  managed_user { 'chen':
+    username => 'chen',
+    ensure   => present,
+  }
 }
