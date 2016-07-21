5a41555a3099d7515bd5d2b6c174379d15a8fcd4
Liz Rosa <liz.rosa@puppet.com>
2016-07-21 22:01:39 +0000

    fix managed_user declarations maybe

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -3,17 +3,17 @@ class users {
     ensure => present,
   }
 
-  managed_user { 'bob':
+  users::managed_user { 'bob':
     username => 'bob',
     ensure   => present,
   }
 
-  managed_user { 'alice':
+  users::managed_user { 'alice':
     username => 'alice',
     ensure   => present,
   }
 
-  managed_user { 'chen':
+  users::managed_user { 'chen':
     username => 'chen',
     ensure   => present,
   }
